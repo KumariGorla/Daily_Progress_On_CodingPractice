daa2296906e538869c97d5275b4ac1995505b27d
kumarigorla <kumarigorla09@gmail.com>
2026-04-14 18:59:22 +0530

    fourth commit

--- Anagram.java
+++ Anagram.java
@@ -1,31 +1,46 @@
 import java.util.*;
+
 class Anagram {
-    public static boolean fun(HashMap<Character,Integer> hma,HashMap<Character,Integer> hmb){
-        if(hma.size()!=hmb.size()) return false;
-        for(char key:hma.keySet()){
-            if(!hmb.containsKey(key)) return false;
-            int a = hma.get(key);
-            int b = hmb.get(key);
-            if(a!=b) return false;
-            
+    public List<Integer> findAnagrams(String s, String p) {
+        
+        HashMap<Character,Integer> hmp = new HashMap<>();
+        for(int i = 0; i < p.length(); i++){
+            char ch = p.charAt(i);
+            hmp.put(ch, hmp.getOrDefault(ch, 0) + 1);
         }
-        return true;
+
+        int l = 0;
+        int k = p.length();
+        List<Integer> ans = new ArrayList<>();
+        HashMap<Character,Integer> hms = new HashMap<>();
+
+        for(int r = 0; r < s.length(); r++){
+            char ch = s.charAt(r);
+            hms.put(ch, hms.getOrDefault(ch, 0) + 1);
+
+            // When window size matches pattern length
+            if(r - l + 1 == k){
+                // Check equality first
+                if(hms.equals(hmp)){
+                    ans.add(l);
+                }
+
+                // Then shrink window
+                char tch = s.charAt(l);
+                hms.put(tch, hms.get(tch) - 1);
+                if(hms.get(tch) == 0){
+                    hms.remove(tch);
+                }
+                l++;
+            }
+        }
+        return ans;
     }
     public static void main(String[] args) {
-        String s1 = "abcdd";
-        String s2 = "bdcda";
-        HashMap<Character,Integer> hma = new HashMap<>();
-        HashMap<Character,Integer> hmb = new  HashMap<>();
-        for(int i = 0;i<s1.length();i++){
-            char ch = s1.charAt(i);
-            hma.put(ch,hma.getOrDefault(ch,0)+1);
-        }
-        for(int j = 0;j<s2.length();j++){
-            char ch1 = s2.charAt(j);
-            hmb.put(ch1,hmb.getOrDefault(ch1,0)+1);
-        }
-        boolean res = fun(hma,hmb);
-        System.out.println(res);
-        
+        Anagram anagram = new Anagram();
+        String s = "cbaebabacd";
+        String p = "abc";
+        List<Integer> result = anagram.findAnagrams(s, p);
+        System.out.println(result); // Output: [0, 6]
     }
 }
